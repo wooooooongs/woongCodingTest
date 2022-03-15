302aab383c0a391cf1d83e492ea4a801194c5c54
wooooooongs <pires11233@gmail.com>
2022-03-16 00:03:07 +0900

    tutorial

---
+++ b1000/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split('\n');
+
+input = input[0];
+input = input.split(' ');
+
+console.log(input);
+
+function solution() {
+}
+
+solution();
