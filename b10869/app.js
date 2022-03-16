b67b4470bb0aa91b9fdc1864c418f9513eb2ac57
wooooooongs <pires11233@gmail.com>
2022-03-17 00:05:02 +0900

    문제풀이

--- b10869/app.js
+++ b10869/app.js
@@ -5,9 +5,12 @@ let input = fs.readFileSync(filePath).toString().split('\n');
 input = input[0];
 input = input.split(' ').map((item) => +item);
 
-solution(input[0], input[1]);
-
 function solution(A, B) {
   console.log(A+B);
+  console.log(A-B);
+  console.log(A*B);
+  console.log(Math.floor(A/B));
+  console.log(A%B);
 }
 
+solution(input[0], input[1]);
