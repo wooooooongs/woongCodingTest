3166cbe4624f6087f17624621d821abf94f13561
wooooooongs <pires11233@gmail.com>
2022-03-21 22:09:11 +0900

    불필요한 것 수정

--- 입출력과 사칙연산/10. b10926/app.js
+++ 입출력과 사칙연산/10. b10926/app.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().trim();
 
-console.log(input + `??!`);
+console.log(input + '??!');
