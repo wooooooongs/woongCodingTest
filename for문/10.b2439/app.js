2b153c80808ed95c54144a8645ba2f0313090e90
wooooooongs <pires11233@gmail.com>
2022-03-27 20:44:46 +0900

    2439문제 내가 푼 건 아님..

--- for문/10.b2439/app.js
+++ for문/10.b2439/app.js
@@ -1,8 +1,18 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-let star = '';
-let blank = '';
+let num = Number(input);
+result = '';
 
-// for(i=1; i<=input; i++){
-//   console.log(star += `*`);
-// }
+for (i=0; i<num; i++){
+  for (j=1; j<num-i; j++){
+    result += ' ';
+  }
+
+  for (k=0; k<=i; k++){
+    result += '*';
+  }
+  result += '\n'
+}
+
+console.log(result);
+
