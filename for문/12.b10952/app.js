68ce3214cef8c69ce72210e2e24f272698ba6f1c
wooooooongs <pires11233@gmail.com>
2022-03-28 15:33:22 +0900

    while문 추가

--- for문/12.b10952/app.js
+++ for문/12.b10952/app.js
@@ -1,9 +1,17 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let num = ''
+let i = 0;
 
-for(i=0; i<input.length-1; i++){
+
+// 1. for문
+// for(i=0; i<input.length-1; i++){
+//   num += +input[i].split(' ')[0] + +input[i].split(' ')[1] + '\n';
+// }
+
+// 2. while문
+while(input[i] !== '0 0'){
   num += +input[i].split(' ')[0] + +input[i].split(' ')[1] + '\n';
+  i++;
 }
-
 console.log(num);
