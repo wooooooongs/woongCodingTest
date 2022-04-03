af016edb416dca32d9c029f31a9ffa489f3e4f9f
wooooooongs <pires11233@gmail.com>
2022-04-03 18:23:51 +0900

    1

--- for문/14.b1110/app.js
+++ for문/14.b1110/app.js
@@ -1,2 +1,8 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
+
+const first = input[0].substring(0,1);
+const second = input[0].substring(1,2);
+const third = +first + +second
+
+console.log(+second + +third);
