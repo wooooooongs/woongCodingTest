c65cfb418c57bcd3fc9733c955bc3f8d6b82c404
wooooooongs <pires11233@gmail.com>
2022-04-09 20:05:51 +0900

    어렵다어려워

--- for문/14.b1110/app2.js
+++ for문/14.b1110/app2.js
@@ -1,7 +1,17 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim()
+let input = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
 
-let one = +input % 10
-let ten = Math.floor(input/10)
+let num = input;
+let sum;
+let cycle = 0;
 
-console.log(one);
-console.log(ten);
+while(true){
+  cycle++;
+  sum = Math.floor(num/10) + num%10; // 14
+  num = (num%10) * 10 + sum%10;
+
+  if(num === input){
+    break;
+  }
+}
+
+console.log(cycle);
