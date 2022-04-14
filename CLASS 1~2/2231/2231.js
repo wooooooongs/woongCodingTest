a9f805ce9751512c28fb56cbd7b0164748e6d997
wooooooongs <pires11233@gmail.com>
2022-04-14 21:03:43 +0900

    2231번 문제풀이

--- CLASS 1~2/2231/2231.js
+++ CLASS 1~2/2231/2231.js
@@ -1,16 +1,19 @@
 const input = require('fs').readFileSync('/dev/stdin');
 
 let arr = [];
+let answer = 0;
 
-for(i=1; i<input; i++){
-  let n1 = i.toString().split('').map((n)=>+n)
+for(i=1; i<=input; i++){
   let calc = 0;
+  let n1 = i.toString().split('').map((n)=>+n)
   
   for(j=0; j<n1.length; j++){
     calc += n1[j]
   }
   calc += i
   calc == input ? arr.push(i) : false;
+
+  arr[0] === undefined ? answer = 0 : answer = arr[0]
 }
 
-console.log(input == 0 ? 0 : arr[0]);
+console.log(answer);
