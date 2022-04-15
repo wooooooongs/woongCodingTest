ce90d133f3d3b4b60299b7e09ab778abd9dc4ed4
wooooooongs <pires11233@gmail.com>
2022-04-15 23:03:36 +0900

    풀이2 추가

--- 프로그래머스/Lv.1/1.직사각형 별찍기/app2.js
+++ 프로그래머스/Lv.1/1.직사각형 별찍기/app2.js
@@ -2,14 +2,10 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let answer = '';
+
+    const line = '*'.repeat(a);
 
     for(i=0; i<b; i++){
-      for(j=0; j<a; j++){
-        answer += '*';
-      }
-      answer += '\n'
+      console.log(line);
     }
-
-    console.log(answer.trim());
-});
+  });
