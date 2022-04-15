d49f8449ca91c26bc7ff00cb111e3d9c4e6f4026
wooooooongs <pires11233@gmail.com>
2022-04-15 23:59:12 +0900

    풀이2

--- 프로그래머스/Lv.1/2.x만큼 간격/app2.js
+++ 프로그래머스/Lv.1/2.x만큼 간격/app2.js
@@ -1,3 +1,5 @@
-function solution(x, n) {
-  return Array(n).fill(x).map((v, i) => (i + 1) * v)
-}
+function soluition(x, n){
+  return Array(n).fill(x).map((v, i) => v * (i + 1)
+  )};
+
+console.log(soluition(2,5));
