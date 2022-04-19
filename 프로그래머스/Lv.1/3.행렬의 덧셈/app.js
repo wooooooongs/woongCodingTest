6ac6cde7ff77a65d3923477913e580a08c3395d2
wooooooongs <pires11233@gmail.com>
2022-04-20 01:36:11 +0900

    단순 이중 for문

--- 프로그래머스/Lv.1/3.행렬의 덧셈/app.js
+++ 프로그래머스/Lv.1/3.행렬의 덧셈/app.js
@@ -1,6 +1,12 @@
 function solution(arr1, arr2) {
-  return arr1.length;
-}
+  let answer = [];
 
-console.log(solution([[1,2],[2,3]],[[3,4],[5,6]]));
-console.log(solution([[1],[2]], [[3],[4]]));
+  for(i=0; i<arr1.length; i++){
+    let sum = [];
+    for(j=0; j<arr1[i].length; j++){
+      sum.push(arr1[i][j] + arr2[i][j]);
+    }
+    answer.push(sum);
+  }
+  return answer;
+}
