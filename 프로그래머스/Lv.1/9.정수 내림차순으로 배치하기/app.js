4c131a2a9d56d2a6f41b23101936bbd7ce8d5441
wooooooongs <pires11233@gmail.com>
2022-05-06 23:01:30 +0900

    숫자로 풀기 추가

--- 프로그래머스/Lv.1/9.정수 내림차순으로 배치하기/app.js
+++ 프로그래머스/Lv.1/9.정수 내림차순으로 배치하기/app.js
@@ -1,5 +1,9 @@
-function solution(n) {
+function 문자열로풀기(n) {
   return Number(n.toString().split('').sort((a, b) => { return b - a }).join(''));
 }
 
-solution(11832);
+function 숫자로풀기(n) {
+  const arr = n + '';
+  const answer = arr.split('').sort().reverse().join('');
+  console.log(answer);
+}
