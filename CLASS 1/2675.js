e4bb5f490b9c7a3ccd44304b80bd17b14e537157
wooooooongs <pires11233@gmail.com>
2022-06-09 02:02:34 +0900

    문자열 반복

--- CLASS 1/2675.js
+++ CLASS 1/2675.js
@@ -1,2 +1,12 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const T = input[0]
 
+for(i=1; i<=T; i++){
+  let arr = input[i].split(' ')
+  let str = arr[1];
+  let arr1 = str.split('').map((v)=>{
+    return v.repeat(arr[0])
+  })
+
+  console.log(arr1.join(''));
+}
