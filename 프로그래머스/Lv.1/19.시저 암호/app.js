d16da874d0cf38fabb6258ba4dedd574adf2bdfa
wooooooongs <pires11233@gmail.com>
2022-06-15 03:35:16 +0900

    1차 도전 실패, n = 25일 때 고려하기

---
+++ 프로그래머스/Lv.1/19.시저 암호/app.js
@@ -0,0 +1,26 @@
+function solution(s, n) {
+    let string = s.split('');
+    let answer = '';
+    
+    for(i=0; i<string.length; i++){
+        let v = string[i].charCodeAt(0);
+
+        if(v === 32) answer += ''
+        else v += n
+
+        if(v > 122) v -= 26
+        // v > 122
+        answer += String.fromCharCode(v)
+    }    
+
+    string
+
+    return answer;
+}
+
+console.log(solution('AB', 1)); 
+console.log(solution('a B z', 4)); 
+
+
+// 테스트 4까지는 성공
+// 테스트 5 ~ 13 실패
