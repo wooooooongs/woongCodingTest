26c9f43d1c07cad83b9a59a3d7f50229542572cb
wooooooongs <pires11233@gmail.com>
2022-07-16 00:00:55 +0900

    전개 연산자와 apply 사용

--- 강의/S1/5.최솟값 구하기.js
+++ 강의/S1/5.최솟값 구하기.js
@@ -9,4 +9,22 @@ function solution(arr) {
 }
 
 let arr=[7, 3, 2, 9, 11];
+
+// 전개 연산자
+function solution2(arr) {
+  let answer = Math.min(...arr);
+
+  return answer;
+}
+
+//apply()
+function solution3(arr) {
+  let answer = Math.min.apply(null, arr);
+
+  return answer;
+}
+
+
 console.log(solution(arr)); // 2 출력
+console.log(solution2(arr)); // 2 출력
+console.log(solution3(arr)); // 2 출력
