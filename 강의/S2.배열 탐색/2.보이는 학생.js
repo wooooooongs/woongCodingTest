82e72e83631dd98c5a5e7ab6ed7d205ccb57326e
wooooooongs <pires11233@gmail.com>
2022-07-29 23:12:55 +0900

    for문으로도 풀기 간단

--- 강의/S2.배열 탐색/2.보이는 학생.js
+++ 강의/S2.배열 탐색/2.보이는 학생.js
@@ -13,5 +13,22 @@ function solution(input){
   return answer
 }
 
+// 그냥 for문
+function solution2(input){
+  let answer = 1;
+  let max = 130;
+
+  for(i=1; i<input.length; i++){
+    if(max < input[i]){
+      max = input[i];
+      answer++;
+    }
+  }
+
+  return answer;
+}
+
 let input = [130,135,148,140,145,150,150,153];
+
 console.log(solution(input));
+console.log(solution2(input));
