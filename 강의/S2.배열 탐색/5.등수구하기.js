81261730249e4a3ae679f8aa1825880602725371
wooooooongs <pires11233@gmail.com>
2022-07-30 16:27:53 +0900

    간단한 문제이지만 다양한 풀이

--- 강의/S2.배열 탐색/5.등수구하기.js
+++ 강의/S2.배열 탐색/5.등수구하기.js
@@ -1,3 +1,4 @@
+// 1. 정렬 후 indexOf로 찾기
 function solution(input){
   let answer = []; // 답 넣는 공간
   let sortedArr = [...input].sort((a, b) => b - a);; // 원본 배열 복사
@@ -7,17 +8,40 @@ function solution(input){
   })
   
   return answer;
-  // 오답. 소팅 후 indexOf를 활용해서 찾는 건 좋았지만
-  // 3명이 같은 점수라면 5명임에도 3등까지 있어야 하지만
-  // 이 풀이로는 2, 2, 2, 1, 5 이런 식으로 나온다.
 }
 
+
+// 2. 이중 for문
 function solution2(input){
-  
-  return input;
+  let answer = [];
+
+  for(i=0; i<input.length; i++){
+    let temp = 1;
+
+    for(j=0; j<input.length; j++){
+      if(input[i] < input[j]) temp ++
+    }
+
+    answer.push(temp);
+  }
+
+  return answer;
 }
 
-const input = [92, 89, 92, 100, 76];
+
+// 3. 이중 for문 대신 map과 filter
+function solution3(input){
+  const arr = input.map((i) => input.filter((j) => i < j).length + 1)
+
+  return arr;
+}
+
+const input = [87, 92, 92, 100, 76];
 
 console.log(solution(input));
 console.log(solution2(input));
+console.log(solution3(input));
+
+// 세 풀이 모두 문제에 맞는 답이 나왔다.
+// 하지만 내가 원하는건 4, 2, 2, 1, 5등이 아닌,
+// 3, 2, 2, 1, 4등이 나왔으면 좋겠다.
