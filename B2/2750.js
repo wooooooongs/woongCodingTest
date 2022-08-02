231da93deaab09f684469b91949ba31357d6df47
wooooooongs <pires11233@gmail.com>
2022-08-03 00:29:51 +0900

    sort를 활용한 쉬운 문제. 숫자, 문자열 차이 존재

---
+++ B2/2750.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+let n = input[0];
+
+input.shift();
+input.sort((a, b) => a - b);
+
+for(i=0; i<n; i++){
+  console.log(input[i]);
+};
