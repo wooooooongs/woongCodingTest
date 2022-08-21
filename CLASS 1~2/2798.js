897f7a09b155aa0e192e6e3f054cff9d564df21a
wooooooongs <pires11233@gmail.com>
2022-08-22 02:23:17 +0900

    이제는 쉽게 느껴지는 브루트포스 입문문제

--- CLASS 1~2/2798.js
+++ CLASS 1~2/2798.js
@@ -1,6 +1,25 @@
-const input = `5 21
-5 6 7 8 9`.split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
-const n = input[0].split('')[0];
+const inputArr = input.split("\n");
+const NM = inputArr[0].split(" ").map((v) => +v);
+const N = NM[0];
+const M = NM[1];
+const cards = inputArr[1].split(" ").map((v) => +v);
+let sumArr = [];
+let sum = 0;
+let answer = 0;
 
-console.log(n);
+for (i = 0; i < N; i++) {
+  // 첫번째 카드
+  for (j = i + 1; j < N; j++) {
+    // 두번째 카드
+    for (k = j + 1; k < N; k++) {
+      // 세번째 카드
+      sum = cards[i] + cards[j] + cards[k];
+      if (sum <= M) sumArr.push(sum);
+    }
+  }
+}
+
+sumArr.sort((a, b) => a - b);
+console.log(sumArr[sumArr.length - 1]);
