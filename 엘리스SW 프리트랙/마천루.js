107d48a32b4220be449c4263173f190ef0829dc0
wooooooongs <pires11233@gmail.com>
2022-08-25 12:40:23 +0900

    사소한 수정

--- 엘리스SW 프리트랙/마천루.js
+++ 엘리스SW 프리트랙/마천루.js
@@ -11,3 +11,17 @@ function solution(input) {
 
   return answer.join('\n');
 }
+// push 없이: 비효율적이다.
+function solution2(input) {
+  let answer = "";
+  let a = "";
+
+  for (i = 0; i < input; i++) {
+    a += "*";
+
+    if (i > 4) answer += `*****\n`;
+    else answer += `${a}\n`;
+  }
+
+  return answer.trim();
+}
